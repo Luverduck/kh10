ddc18b7ac93125a5cffaa2d3505c33b4d2dbc25a
hyeul <hyeul@LAPTOP-J0K0DP1F>
2022-07-26 10:40:46 +0900

    설명 추가

--- day01/src/Hello.java
+++ day01/src/Hello.java
@@ -1,8 +1,9 @@
-import java.lang.*;
+import java.lang.*; //import는 준비 명령
 public class Hello {
 	public static void main(String[] args) {
-			System.out.println("Hello java!");
-			System.out.println("Hello java!");
-			//System.out.println("Hello java!");
+		//출력 코드 - 결과 화면에 원하는 글자 출력
+		System.out.println("Hello java!");
+		System.out.println("Hello java!");
+		//System.out.println("Hello java!");
 	}
 }
