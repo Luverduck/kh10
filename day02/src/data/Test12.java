c91a781ee348c654b4c7caa3b56f78f7522c16f1
Luverduck <hyekko92@gmail.com>
2022-07-27 00:15:25 +0900

    정수 실수간 변환 예제

--- day02/src/data/Test12.java
+++ day02/src/data/Test12.java
@@ -10,7 +10,8 @@ public class Test12 {
 
 		//규칙
 		//1. 작은 형태의 데이터는 큰 형태로 저장할 수 있다
-		//2. 큰 형태의 데이터는 작은 형태로 저장할 수 없다
+		//2. 큰 형태의 데이터는 작은 형태로 저장할 수 없다, 데이터 손실 발생할 수 있음
+		//3. 실수는 무조건 정수보다 크다
 
 		int a = 10;
 		long b = a; //long이 더 크므로 int를 long에 저장할 수 있다 (자동 변환)
@@ -19,7 +20,11 @@ public class Test12 {
 		int d = (int)c; // long이 더 크므로 long을 int로 저장할 수 없다 (자동 변환 불가능, 변환 연산시 가능)
 		
 		long e = 10L;
-		float f = e;
+		float f = e; //long -> float 시 자동 변환
+		
+		//float g = 1.5f;
+		//long h = g; //float -> long 시 강제 변환 (자동 변환 불가능)
+		
 	}
 
 }
