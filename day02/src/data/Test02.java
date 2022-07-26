ac854e35f96e4e4f714002cbc63fe40d3ac32d16
Luverduck <hyekko92@gmail.com>
2022-07-26 11:34:35 +0900

    중국집 금액 계산기

---
+++ day02/src/data/Test02.java
@@ -0,0 +1,9 @@
+package data;
+// (Q) 자장면 5천원, 짬뽕 6천원일 때 자장면 2그릇 + 짬뽕 3그릇 가격?
+import java.lang.*;
+public class Test02 {
+	public static void main(String[] args) {
+		System.out.println(5000 * 2 + 6000 * 3);
+	}
+
+}
