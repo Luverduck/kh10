a2986beadc5a8233a7dee752e34d9ca8bfc86f7c
Luverduck <hyekko92@gmail.com>
2022-07-28 14:21:53 +0900

    System.in을 단독으로 쓰지 않는 이유

--- day04/src/io/Test01.java
+++ day04/src/io/Test01.java
@@ -9,14 +9,14 @@ public class Test01 {
 		//표준 입력 - System.in 을 이용하여 글자를 받아들이는 행위
 		
 		//System.out.println(); - 화면에 글자를 내보내고 줄을 바꾸는 명령 (print + line)
-		System.out.println("Hello"); 
-		System.out.println("Hello");
+		System.out.println("Hello1"); 
+		System.out.println("Hello1");
 		
 		//System.out.print(); - 화면에 글자를 내보내는 명령 (줄바꿈 x)
-		System.out.print("Hello"); 
-		System.out.print("Hello");
+		System.out.print("Hello2"); 
+		System.out.print("Hello2");
 		System.out.print("\n"); //직접 줄바꿈을 하는 것
-		System.out.print("Hello");
+		System.out.print("Hello2");
 		System.out.print("\n");
 		
 		//System.out.printf(); - C언어 개발자를 위해 만든 출력 명령
