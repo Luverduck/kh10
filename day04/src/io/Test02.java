1260d19ecf2ec909f4bf4b58925f7b3ac08eec1f
Luverduck <hyekko92@gmail.com>
2022-07-28 14:22:50 +0900

    표준 출력 명령

--- day04/src/io/Test02.java
+++ day04/src/io/Test02.java
@@ -7,7 +7,7 @@ public class Test02 {
 	public static void main(String[] args) throws IOException {
 		
 		//System.in 만을 사용하여 입력을 받는 예제
-		//(문제점)
+		//(문제점) 
 		//1. 하나의 명령이 한 글자만 입력받는다
 		//2. 입력을 아스키코드표 숫자로 변화해서 받는다
 		//3. 예외 처리를 해야 한다 - throws IOException {
