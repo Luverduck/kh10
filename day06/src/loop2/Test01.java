993febf2be1ae1d189ac7de41f0715cde9437b68
Luverduck <hyekko92@gmail.com>
2022-08-01 16:42:21 +0900

    중첩 반복문

--- day06/src/loop2/Test01.java
+++ day06/src/loop2/Test01.java
@@ -17,7 +17,7 @@ public class Test01 {
 		for(int hour = 0; hour < 24 ; hour++) {
 			for(int min = 0; min < 60; min++) {
 				for(int sec = 0; sec < 60; sec++) {
-					System.out.println(hour + "시" + min + "분 " + sec + "초");
+					System.out.println(hour + "시 " + min + "분 " + sec + "초");
 				}
 			}
 		}
