e97ef50a361b1d40a435e69c6744ffc33d9f6232
Luverduck <hyekko92@gmail.com>
2022-08-04 10:48:26 +0900

    랜덤 빙고판

---
+++ day08/src/array2d/Test07.java
@@ -0,0 +1,57 @@
+package array2d;
+
+//다음 규칙에 따라 빙고판을 만드세요
+//- 5x5 크기의 정수 배열을 준비합니다
+//- 1부터 25까지의 숫자를 무작위 위치에 배치합니다
+//- 한 칸에는 한 번만 값을 배치할 수 있습니다
+//- 크기가 달라질 경우에도 달라진 크기에 맞게 빙고판이 만들어져야 합니다
+//ex
+// 20	13	5	14	19
+// 6	12	1	8	25
+// 21	2	9	15	18
+// 22	10	16	3	7
+// 11	24	4	17	23
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Test07 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		//System.out.println("숫자를 입력하세요");
+		//int number = sc.nextInt();
+		
+		int number = 5;
+	
+		int[][] array = new int[number][number];
+		
+		Random r = new Random();
+		
+		//중복 번호에 대해서 어떻게 해결할 것인지?
+		
+		//값 기준으로 하기 => count를 이용하여 1부터 25까지 랜덤 위치에 대해 설명
+		for(int i = 1 ; i < 25 ; i ++) {
+			int x = r.nextInt(5);
+			int y = r.nextInt(5);
+			if(array[x][y] == 0) {	//0이 들어가있다면 데이터 입력
+				array[x][y] = i;
+			}
+			else {					//0이 들어있지 않다면 재추첨 방법 중요
+				i--;				//이전으로 돌아가(i를 감소) 다시 추첨 (재추첨)	(for 반복문에서만 가능한 방법)
+			}
+		}
+		
+		for(int j = 0 ; j < array.length ; j ++) {
+			for(int i = 0 ; i < array[j].length ; i++) {
+				System.out.print(array[j][i]);
+				System.out.print("\t");
+			}
+			System.out.println();
+			System.out.println();
+		}
+	
+	}
+
+}
