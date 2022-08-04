5db6ede629ff9a1a0f8ae604dd079fe71020d49d
Luverduck <hyekko92@gmail.com>
2022-08-04 11:17:23 +0900

    배열 순서대로 숫자 넣기

---
+++ day08/src/array2d/Test04.java
@@ -0,0 +1,40 @@
+package array2d;
+
+//사용자에게 줄 수와 칸 수를 입력받아 해당하는 크기만큼의 2차원 배열을 생성하고
+//좌측 상단에 1부터 채워넣어 끝까지 1씩 증가하며 숫자가 채워지도록 구현하세요
+//구현 후 배열을 출력하세요
+//줄 수 : 3, 칸 수 : 4
+//1	2	3	4
+//5	6	7	8
+//9	10	11	12
+//추가> 줄별로 합계를 구하세요
+
+public class Test04 {
+	public static void main(String[] args) {
+		//준비
+		int row = 3;
+		int col = 4;
+		
+		int[][] arr = new int[row][col];
+		
+		//계산
+		//- (줄,칸) 기준
+		//(0,0)→(1,0)→(2,0)→(0,1)→(1,1)→(2,1)→(0,2)→(1,2)→(2,2)→(0,3)→(1,3)→(2,3)
+		int n = 1;
+		for(int i=0; i < col; i++) {
+			for(int k=0; k < row; k++) {
+				arr[k][i] = n;
+				n++;
+			}
+		}
+		
+		//출력
+		for(int i=0; i < arr.length; i++) {
+			for(int k=0; k < arr[i].length; k++) {
+				System.out.print(arr[i][k]);
+				System.out.print("\t");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ day08/src/array2d/Test04_2.java
@@ -0,0 +1,48 @@
+package array2d;
+
+//사용자에게 줄 수와 칸 수를 입력받아 해당하는 크기만큼의 2차원 배열을 생성하고
+//좌측 상단에 1부터 채워넣어 끝까지 1씩 증가하며 숫자가 채워지도록 구현하세요
+//구현 후 배열을 출력하세요
+//줄 수 : 3, 칸 수 : 4
+//1	2	3	4
+//5	6	7	8
+//9	10	11	12
+//추가> 줄별로 합계를 구하세요
+
+public class Test04_2 {
+	public static void main(String[] args) {
+		//준비
+		int row = 3;
+		int col = 4;
+		
+		int[][] arr = new int[row][col];
+		
+		//계산
+		//- 값 기준
+		//1→2→3→...→12(row*col)
+		int x = 0;
+		int y = 0;
+		for(int i=1; i <= row*col; i++) {
+			//System.out.println(i+" → ("+x+", "+y+")");
+			arr[x][y] = i;
+			
+			if(x < row-1) {//마지막 줄이 아니라면
+				x++;//다음줄
+			}
+			else {//마지막 줄이라면
+				//다음 칸 첫번째 줄로 이동
+				y++;
+				x = 0;
+			}
+		}
+		
+		//출력
+		for(int i=0; i < arr.length; i++) {
+			for(int k=0; k < arr[i].length; k++) {
+				System.out.print(arr[i][k]);
+				System.out.print("\t");
+			}
+			System.out.println();
+		}
+	}
+}
