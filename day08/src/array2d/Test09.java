ee80d25c25b9b378559f192ce73eb5453c8763a2
Luverduck <hyekko92@gmail.com>
2022-08-04 11:23:14 +0900

    성적 문제 풀이

--- day08/src/array2d/Test09.java
+++ day08/src/array2d/Test09.java
@@ -1,63 +1,51 @@
 package array2d;
 
-//다음은 우리반 학생의 과목별 성적 정보입니다
-//이름을 제외한 나머지 정보들을 단 하나의 2차원 배열로 저장하고 싶습니다.
-//총점과 평균과 순위는 최초 0으로 설정한 뒤 계산을 통해서 채운 뒤 출력하려고 합니다
-//프로그래밍 코드를 작성하여 모든 정보를 채우고 출력하세요
-
 public class Test09 {
-	
 	public static void main(String[] args) {
+		//준비
+		int[][] data = new int[][] {
+			{100, 90, 80, 0, 0, 0},
+			{95, 92, 93, 0, 0, 0},
+			{70, 95, 95, 0, 0, 0}
+		};
 		
-		int[][] array = new int[3][6];
-		
-		array[0][0] = 100;
-		array[0][1] = 90;
-		array[0][2] = 80;
-		array[0][3] = 0;
-		array[0][4] = 0;
-		array[0][5] = 0;
-		
-		
-		array[1][0] = 95;
-		array[1][1] = 92;
-		array[1][2] = 93;
-		array[1][3] = 0;
-		array[1][4] = 0;
-		array[1][5] = 0;
-		
-		
-		array[2][0] = 70;
-		array[2][1] = 95;
-		array[2][2] = 95;
-		array[2][3] = 0;
-		array[2][4] = 0;
-		array[2][5] = 0;
-		
-		
-		//합계 및 평균
-		for(int j = 0 ; j < array.length ; j ++) {	//위에서 아래로 1번
-			
-			int sum = 0;
-			int avg = 0;
-			for(int i = 0 ; i < array[j].length - 3 ; i++) {
-				sum += array[j][i];										//합계와 평균을 동시에?
-				avg = sum / 3;
-				array[j][3] = sum;
-				array[j][4] = avg;
+		//계산
+		//[1] 총점
+		for(int i=0; i < data.length; i++) {
+			int total = 0;
+			for(int k=0; k < 3; k++) {
+				total += data[i][k];
 			}
+			data[i][3] = total;
 		}
-
+		
+		//[2] 평균
+		for(int i=0; i < data.length; i++) {
+			int average = data[i][3] / 3;
+			data[i][4] = average;
+		}
+		
+		//[3] 순위
+		for(int i=0; i < data.length; i++) {
+			int rank = 1;
+			int score = data[i][3];
+			for(int k=0; k < data.length; k++) {
+				if(score > data[k][3]) {
+					rank++;
+				}
+			}
+			data[i][5] = rank;
+		}
 		
 		//출력
-		for(int j = 0 ; j < array.length ; j ++) {
-			for(int i = 0 ; i < array[j].length ; i++) {
-				System.out.print(array[j][i]);
+		System.out.println("국어\t영어\t수학\t총점\t평균\t순위");
+		for(int i=0; i < data.length; i++) {
+			for(int k=0; k < data[i].length; k++) {
+				System.out.print(data[i][k]);
 				System.out.print("\t");
 			}
 			System.out.println();
+			System.out.println();
 		}
-		
 	}
-
 }
