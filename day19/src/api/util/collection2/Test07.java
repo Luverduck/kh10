58272bb06d2564795eb682ad5a134517c7f1a712
Luverduck <hyekko92@gmail.com>
2022-08-19 12:09:09 +0900

    Set의 집합 연산 - 합집합, 교집합, 차집합

--- day19/src/api/util/collection2/Test07.java
+++ day19/src/api/util/collection2/Test07.java
@@ -1,14 +1,14 @@
 package api.util.collection2;
 
-import java.util.Set;
-import java.util.TreeSet;
-
 // 다음 요구사항에 따른 계산을 수행하여 결과를 출력하시오.
 // 철수와 영희는 각자 국내에서 상영하는 영화를 다음과 같이 봤습니다.
 // 철수 : 이상한 나라의 수학자, 더 배트맨, 인민을 위해 복무하라, 블랙라이트
 // 영희 : 더 배트맨, 스파이더맨 : 노웨이 홈, 블랙라이트, 우리가 사랑이라고 믿는 것
 // (Q) 철수와 영희가 둘 다 본 영화 목록을 출력하세요
 // (Q) 철수와 영희 중 한 명만 본 영화 목록을 출력하세요
+
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Test07 {
 
