b2e0d57f8cd7627087f496a320181e78c6801928
Luverduck <hyekko92@gmail.com>
2022-08-19 12:08:19 +0900

    Set의 집합 연산 예제 - 둘 다 본 영화 / 둘 중 한명만 본 영화

---
+++ day19/src/api/util/collection2/Test07.java
@@ -0,0 +1,44 @@
+package api.util.collection2;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+// 다음 요구사항에 따른 계산을 수행하여 결과를 출력하시오.
+// 철수와 영희는 각자 국내에서 상영하는 영화를 다음과 같이 봤습니다.
+// 철수 : 이상한 나라의 수학자, 더 배트맨, 인민을 위해 복무하라, 블랙라이트
+// 영희 : 더 배트맨, 스파이더맨 : 노웨이 홈, 블랙라이트, 우리가 사랑이라고 믿는 것
+// (Q) 철수와 영희가 둘 다 본 영화 목록을 출력하세요
+// (Q) 철수와 영희 중 한 명만 본 영화 목록을 출력하세요
+
+public class Test07 {
+
+	public static void main(String[] args) {
+		
+		// 철수의 Set 생성
+		Set<String> chul = new TreeSet<>();
+		chul.add("이상한 나라의 수학자");
+		chul.add("더 배트맨");
+		chul.add("인민을 위해 복무하라");
+		chul.add("블랙라이트");
+		
+		// 영희의 Set 생성
+		Set<String> young = new TreeSet<>();
+		young.add("더 배트맨");
+		young.add("스파이더맨 : 노웨이 홈");
+		young.add("블랙라이트");
+		young.add("우리가 사랑이라고 믿는 것");
+		
+		// 둘 다 본 영화 (교집합)
+		Set<String> common = new TreeSet<>();
+		common.addAll(chul);
+		common.retainAll(young);
+		System.out.println("둘 다 본 영화 : " + common);
+		
+		// 둘 중 한명만 본 영화 (철수와 영희의 합집합 - 교집합)
+		Set<String> complement = new TreeSet<>();
+		complement.addAll(chul);
+		complement.addAll(young);
+		complement.removeAll(common);
+		System.out.println("둘 중 한명만 본 영화 : " + complement);
+	}
+}
