d6b0e58b9f7809a424524a5a9e954666a822f91b
Luverduck <hyekko92@gmail.com>
2022-08-29 11:14:05 +0900

    도서 정보 조회(select) - 조건에 맞는 데이터만 조회 (번호) - mapper에서 bookDto.setBookGenre(rs.getString("book_genre")); 추가 - RowMapper를 생성하지 않고 DTO에 있는 RowMapper를 불러오는 방식으로 변경

--- jdbc04/src/app/Test05.java
+++ jdbc04/src/app/Test05.java
@@ -15,7 +15,7 @@ public class Test05 {
 
 	public static void main(String[] args) {
 		
-		// app.Test05 - 사용자가 입력한 번호에 해당하는 도서의 정보를 출력
+		// app.Test05 - 사용자가 입력한 번호에 해당하는 도서의 정보를 출력 -> 단일 조회
 		
 		// 1. template 불러오기
 		JdbcTemplate template = JdbcUtil.getTemplate();
@@ -32,6 +32,7 @@ public class Test05 {
 		Object[] param = new Object[] {bookSerial};
 		
 		// 4. RowMapper 생성
+		/*
 		RowMapper<BookDto> mapper = new RowMapper<BookDto>() {
 			@Override
 			public BookDto mapRow(ResultSet rs, int idx) throws SQLException {
@@ -46,13 +47,20 @@ public class Test05 {
 				return bookDto;
 			}
 		};
+		*/
 		
-		// 5. List 생성
-		List<BookDto> list = template.query(sql, mapper, param);
+		// 5. List 생성 - 단일 조회에서는 List를 생성하지 않음
+		
+		// 5. BookDto의 인스턴스 생성
+		BookDto bookDto = template.query(sql, BookDto.getExtractor(), param);
 		
 		// 6. 출력
-		for(BookDto bookDto : list) {
-			System.out.println(bookDto);
+		if(bookDto == null) {
+			System.out.println("존재하지 않는 도서 번호");
+		}
+		else {
+			System.out.println(bookSerial + "번 도서 정보");
+			System.out.println(bookSerial);
 		}
 		
 		sc.close();
