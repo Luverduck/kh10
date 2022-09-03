a2e60ee69afebc1b7e96c3e133a1f35154437c67
Luverduck <hyekko92@gmail.com>
2022-09-03 11:33:22 +0900

    JSP 기본 설명

---
+++ spring11view/src/main/java/com/kh/spring11/controller/ViewController.java
@@ -0,0 +1,52 @@
+package com.kh.spring11.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tag")
+public class ViewController {
+
+	@RequestMapping("/test01")
+	public String test01() {
+//		return "/WEB-INF/views/tag/test01.jsp";
+		return "tag/test01";
+	}
+	
+	@RequestMapping("/test02")
+	public String test02() {
+		return "tag/test02";
+	}
+	
+	@RequestMapping("/test03")
+	public String test03() {
+		return "tag/test03";
+	}
+	
+	@RequestMapping("/test04")
+	public String test04() {
+		return "tag/test04";
+	}
+	
+	@RequestMapping("/test05")
+	public String test05() {
+//		viewresolver 세팅
+//		return "/WEB-INF/views/tag/test05.jsp";
+		return "tag/test05";
+	}
+	
+	@RequestMapping("/test06")
+	public String test06() {
+		return "tag/test06";
+	}
+	
+	@RequestMapping("/test07")
+	public String test07() {
+		return "tag/test07";
+	}
+	
+	@RequestMapping("/test08")
+	public String test08() {
+		return "tag/test08";
+	}
+}
