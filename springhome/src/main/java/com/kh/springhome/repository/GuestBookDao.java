3e38b00e67226b6e9b39e25a8855f0ddb2b2e37d
Luverduck <hyekko92@gmail.com>
2022-09-04 16:08:50 +0900

    GuestBookDao 인터페이스 생성 및 insert() 추상 메소드 선언

---
+++ springhome/src/main/java/com/kh/springhome/repository/GuestBookDao.java
@@ -0,0 +1,9 @@
+package com.kh.springhome.repository;
+
+import com.kh.springhome.entity.GuestBookDto;
+
+public interface GuestBookDao {
+
+	// 등록(insert)
+	void insert(GuestBookDto guestBookDto);
+}
