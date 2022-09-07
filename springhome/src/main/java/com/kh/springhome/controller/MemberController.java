5b0a18d891bd64562c8844fa64539766b9933f93
Luverduck <hyekko92@gmail.com>
2022-09-07 10:41:31 +0900

    MemberController 생성 - 조회 추가

--- springhome/src/main/java/com/kh/springhome/controller/MemberController.java
+++ springhome/src/main/java/com/kh/springhome/controller/MemberController.java
@@ -2,10 +2,12 @@ package com.kh.springhome.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.springhome.entity.MemberDto;
 import com.kh.springhome.repository.MemberDao;
@@ -34,5 +36,22 @@ public class MemberController {
 	public String insertSuccess() {
 //		return "/WEB-INF/views/member/insertSuccess";
 		return "member/insertSuccess";
+	}
+	
+	@GetMapping("/list")
+	public String SelectList(
+								Model model, 
+								@RequestParam(required = false) String type, 
+								@RequestParam(required = false) String keyword) {
+		boolean isSearch = type != null && keyword != null;
+		if(isSearch) {
+			model.addAttribute("list", memberDao.selectList(type, keyword));
+		}
+		else {
+			model.addAttribute("list", memberDao.selectList());
+		}
+		// return "목록 페이지 주소";
+		// return "/WEB-INF/views/member/list.jsp";
+		return "member/list";
 	}
 }
