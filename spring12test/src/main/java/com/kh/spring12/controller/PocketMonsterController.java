ce351ff74b18c5cc802a14dc779c6c23b98458f6
Luverduck <hyekko92@gmail.com>
2022-09-15 16:58:16 +0900

    MockMvc를 이용한 컨트롤러 단위테스트

---
+++ spring12test/src/main/java/com/kh/spring12/controller/PocketMonsterController.java
@@ -0,0 +1,30 @@
+package com.kh.spring12.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.spring12.entity.PocketMonsterDto;
+import com.kh.spring12.repository.PocketMonsterDao;
+
+@Controller
+@RequestMapping("/pocketmon")
+public class PocketMonsterController {
+
+	@GetMapping("/insert")
+	public String insert() {
+		return "pocketmon/insert";
+	}
+	
+	@Autowired
+	private PocketMonsterDao pocketMonsterDao;
+	
+	@PostMapping("/insert")
+	public String insert(@ModelAttribute PocketMonsterDto pocketMonsterDto) {
+		pocketMonsterDao.insert(pocketMonsterDto);
+		return "redirect:insert";
+	}
+}
