b6d1abcbd7a61cecebbb3e769d92d709714fbaa1
Luverduck <hyekko92@gmail.com>
2022-09-15 16:56:14 +0900

    단위 테스트코드 예제

--- spring12test/src/test/java/com/kh/spring12/Test02.java
+++ spring12test/src/test/java/com/kh/spring12/Test02.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-
+ 
 @SpringBootTest
 public class Test02 {
 
