b7066abdde28247d615097134948c3c65f24e761
Luverduck <hyekko92@gmail.com>
2022-11-09 17:55:43 +0900

    마임메세지 기본 예제

---
+++ spring18email/src/test/java/com/kh/spring18/MineMessageTest1.java
@@ -0,0 +1,37 @@
+package com.kh.spring18;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+@SpringBootTest
+public class MineMessageTest1 {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	@Test
+	public void test() throws MessagingException {
+		// 1. 메세지 생성
+		MimeMessage message = javaMailSender.createMimeMessage();
+				
+		// 2. 헬퍼퍼 생성
+		MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
+		
+		// 3. 정보 설정
+		helper.setTo("eomhyunyoung@gmail.com");
+		helper.setSubject("마임메세지 테스트");
+		// html 태그를 쓰지 않을 경우
+		// helper.setText("<h1>안녕</h1>");
+		// html 태그를 쓸 경우
+		helper.setText("<h1>안녕</h1>", true);
+		
+		// 4. 전송
+		javaMailSender.send(message);
+	}
+}
