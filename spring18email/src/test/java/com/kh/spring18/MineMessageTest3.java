bc8b84f8b0af5f74e3efd13726413ecec4da185f
Luverduck <hyekko92@gmail.com>
2022-11-10 10:16:42 +0900

    마임 메세지 첨부파일 및 템플릿 적용

--- spring18email/src/test/java/com/kh/spring18/MineMessageTest3.java
+++ spring18email/src/test/java/com/kh/spring18/MineMessageTest3.java
@@ -1,6 +1,9 @@
 package com.kh.spring18;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
@@ -10,6 +13,7 @@ import javax.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
@@ -20,22 +24,27 @@ public class MineMessageTest3 {
 	private JavaMailSender javaMailSender;
 	
 	@Test
-	public void test() throws MessagingException {
+	public void test() throws MessagingException, FileNotFoundException, IOException {
 		// 1. 메세지 생성
 		MimeMessage message = javaMailSender.createMimeMessage();
 				
-		// 2. 헬퍼퍼 생성 - 파일 첨부시 multipart 부분을 true로 한다
-		MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+		// 2. 헬퍼 생성 - 파일 첨부시 multipart 부분을 true로 한다
+		MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
 		
 		// 3. 정보 설정
 		helper.setTo("eomhyunyoung@gmail.com");
-		helper.setSubject("첨부파일 테스트");
-		helper.setText("첨부파일을 확인해주세요");
+		helper.setSubject("HTML 템플릿 테스트");
 		
-		// 이메일 첨부는 DataSource라는 형태로 해야한다
-		File target = new File("D:\\", "attachmentTest.jpg");
-		DataSource source = new FileDataSource(target); // activation에 있는 것으로 import
-		helper.addAttachment(target.getName(), source); // 원본 파일명으로 전송
+		ClassPathResource resource = new ClassPathResource("email/template2.html");
+		
+		StringBuffer buffer = new StringBuffer();
+		try(Scanner sc = new Scanner(resource.getFile())){ // scanner 자동 close
+			while(sc.hasNextLine()) {
+				buffer.append(sc.nextLine());
+			}
+		}
+		
+		helper.setText(buffer.toString(), true); // html 모드 true
 		
 		// 4. 전송
 		javaMailSender.send(message);
